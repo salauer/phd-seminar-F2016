936c7100bb522e741848ca20f96e99d4982bb7ea
Sara Marie Nunez <snunez@1x-nat-vl932-172-30-225-34.wireless.umass.edu>
2016-10-12 12:41:23 -0400

    first meeting code

--- code/initial-investigation.R
+++ code/initial-investigation.R
@@ -4,9 +4,38 @@
 library(cdcfluview)
 library(ggplot2)
 library(readr)
-
-usflu <- get_flu_data("national", "ilinet", years=1997:2010)
-write.csv(usflu, "data/usflu.csv")
+library()
+#usflu <- get_flu_data("national", "ilinet", years=1997:2010)
+#write.csv(usflu, "data/usflu.csv")
 usflu <- read_csv("data/usflu.csv", na="X")
 
 qplot(data=usflu, x=YEAR+(WEEK-1)/53,y=X.UNWEIGHTED.ILI, geom="line")
+
+usflu2 <- usflu[,c("X1","X.UNWEIGHTED.ILI")]
+usflu3 <- usflu2[usflu$YEAR>=2003,]
+
+## ORIGINAL
+par(mfrow=c(2,1))
+acf(usflu3$X.UNWEIGHTED.ILI,lag.max = 53)
+acf(usflu3$X.UNWEIGHTED.ILI,lag.max = 53,type="partial")
+
+## FIRST DIFFERENCE
+dflu <- diff(usflu3$X.UNWEIGHTED.ILI)
+acf(dflu,lag.max= 53)
+acf(dflu,lag.max= 53,type=c("partial"))
+
+## FIRST DIFFERENCE qplot
+dflu_dat <- cbind(usflu3$X1,dflu)
+qplot(x=usflu3$X1[-c(1)],y=dflu, geom="line")
+
+## SEASONAL DIFFERENCING
+ddflu <- diff(diff(usflu3$X.UNWEIGHTED.ILI),52)
+acf(ddflu,lag.max = 53)
+acf(ddflu,lag.max = 53,type="partial")
+
+## ALL PLOTS
+par(mfrow=c(2,2))
+acf(dflu,lag.max= 53)
+acf(dflu,lag.max= 53,type=c("partial"))
+acf(ddflu,lag.max = 53)
+acf(ddflu,lag.max = 53,type="partial")
